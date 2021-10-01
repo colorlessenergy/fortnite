7a8bcd1021118fff06bf39e92256f9871d8b5ed3
colorlessenergy <brianmunozdev@gmail.com>
2021-10-01 12:24:03 -0700

    add HTML structure for nav menu inner

--- shared/components/NavMenu.js
+++ shared/components/NavMenu.js
@@ -18,7 +18,11 @@ const NavMenu = ({ isNavMenuOpen }) => {
 				<label className="d-none" htmlFor="search">
 					search
 				</label>
-				<input className="nav-menu__input p-1" type="text" autoComplete="off" placeholder="Search..." />
+				<input
+					className="nav-menu__input p-1"
+					type="text"
+					autoComplete="off"
+					placeholder="Search..." />
 
 				<button className="nav-menu__button">
 					<i className="las la-search"></i>
@@ -39,8 +43,34 @@ const NavMenu = ({ isNavMenuOpen }) => {
 					<i className="las la-globe"></i>
 				</div>
 			</div>
+
+			<div className="nav-menu nav-menu--inner nav-menu-animate">
+				<div className="nav-menu__title cursor-pointer text-uppercase px-3 py-1 flex">
+					<i class="las la-angle-left"></i>
+					<div className="flex-1 text-medium flex align-items-center justify-content-center">
+						modes
+					</div>
+				</div>
+				<ul className="flex-1">
+					<li>
+						<a className="text text-uppercase text-gray py-1 px-3 nav-menu__item" href="/">
+							battle royale
+						</a>
+					</li>
+					<li>
+						<a className="text text-uppercase text-gray py-1 px-3 nav-menu__item" href="/">
+							creative
+						</a>
+					</li>
+					<li>
+						<a className="text text-uppercase text-gray py-1 px-3 nav-menu__item" href="/">
+							save the world
+						</a>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
-};
+}
 
 export default NavMenu;
