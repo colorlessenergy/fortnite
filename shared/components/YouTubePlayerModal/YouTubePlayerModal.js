330a7d3d377a223402c9646eef32159f2cf22dc1
colorlessenergy <brianmunozdev@gmail.com>
2021-10-02 18:21:23 -0700

    add functionality to toggle youtube player modal

--- shared/components/YouTubePlayerModal/YouTubePlayerModal.js
+++ shared/components/YouTubePlayerModal/YouTubePlayerModal.js
@@ -1,13 +1,17 @@
 import classes from './YouTubePlayerModal.module.scss';
 
-const YouTubePlayerModal = ({ link }) => {
+const YouTubePlayerModal = ({ link, toggleYoutubePlayerModal }) => {
     return (
-        <div className="overlay flex justify-content-center align-items-center">
+        <div
+            onClick={ toggleYoutubePlayerModal }
+            className="overlay flex justify-content-center align-items-center">
             <div className="position-relative">
-                <button className={ classes["close-button"] }>
+                <button
+                    onClick={ toggleYoutubePlayerModal }
+                    className={ classes["close-button"] }>
                     <i className="las la-times"></i>
                 </button>
-                <iframe width="560" height="315" src={ link } frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="560" height="315" src={ link } frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
         </div>
     );
