a59bc62b93093aa43fc398674d31888312deaec5
colorlessenergy <brianmunozdev@gmail.com>
2021-10-06 18:43:24 -0700

    modify 2nd element in carousel positioning

--- pages/index.js
+++ pages/index.js
@@ -110,7 +110,7 @@ export default function Home ({ posts }) {
                                     alt="season logo" /> 
                             </div>
 
-                            <div className="drop-shadow-pink h-100 flex flex-direction-column justify-content-center font-thick text-uppercase color-white carousel-padding">
+                            <div className="drop-shadow-pink h-100 flex flex-direction-column justify-content-center justify-content-end justify-content-center-desktop carousel__padding-bottom font-thick text-uppercase color-white">
                                 <div className="text-center carousel__second-wrapper">
                                     <div className="font-italic text-large mb-1">
                                         season 8
